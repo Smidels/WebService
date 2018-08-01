e02e098ecdf01b2e687aba9da54def35f84ba63a
Smidels <voylok5@gmail.com>
2018-08-01 17:55:23 +0300

    First successful launch

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,11 @@
-from flask_wtf import FlaskFrom
-from wtform import StringField, PasswordField, BooleanField, SubmitField
-from wtform.validators import DataRequired
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 
-class LoginForm(FlaskFrom):
+
+class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('password', validators=[DataRequired()])
-	rememder_me = BooleanField('Remember Me')
+	remember_me = BooleanField('Remember Me')
 	submit = SubmitField('Sign In')
